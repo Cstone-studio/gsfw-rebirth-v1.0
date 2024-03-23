1b5af02988bb7f1b30febfc0a47aa4d2237d50ee
liuchengyao <31797319+Cstone-studio@users.noreply.github.com>
2024-03-23 12:31:46 +0800

    junit test

---
+++ src/test/java/com/gs/GsTest.java
@@ -0,0 +1,14 @@
+package com.gs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GsTest {
+
+    @Test
+    public void test() {
+        System.out.println("test start");
+    }
+}
