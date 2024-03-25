2aefe1db1318ca22f19bfcb36bb957226fa29cf7
liuchengyao <31797319+Cstone-studio@users.noreply.github.com>
2024-03-25 12:49:45 +0800

    解决 junit 中无法依赖注入的问题

--- src/test/java/com/gs/GsTest.java
+++ src/test/java/com/gs/GsTest.java
@@ -1,11 +1,16 @@
 package com.gs;
 
+import com.gs.service.intf.DemoUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class GsTest {
+
+    private DemoUserService demoUserService;
 
     @Test
     public void test() {
